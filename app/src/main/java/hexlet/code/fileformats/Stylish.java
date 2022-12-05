d2cf1cc60a053da6cec8261d3afa247802633f06
Kupchinskii Evgenii <kupchinskiievgenii81@gmail.com>
2022-12-05 16:16:49 +0300

    update

---
+++ app/src/main/java/hexlet/code/fileformats/Stylish.java
@@ -0,0 +1,33 @@
+package hexlet.code.fileformats;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.SimpleTimeZone;
+
+public class Stylish {
+    public static String stylishFormat(Map<String, Map<String, Object>> fileDifference) throws IOException {
+        StringBuilder sb = new StringBuilder("{\n");
+        for (Map.Entry<String, Map<String, Object>> mapEntry : fileDifference.entrySet()) {
+            if (fileDifference.get(mapEntry.getKey()).get("type").equals("added")) {
+                sb.append("  + ").append(mapEntry.getKey()).append(": ")
+                        .append(fileDifference.get(mapEntry.getKey()).get("newValue")).append("\n");
+            }
+            if (fileDifference.get(mapEntry.getKey()).get("type").equals("removed")) {
+                sb.append("  - ").append(mapEntry.getKey()).append(": ")
+                        .append(fileDifference.get(mapEntry.getKey()).get("oldValue")).append("\n");
+            }
+            if (fileDifference.get(mapEntry.getKey()).get("type").equals("changed")) {
+                sb.append("  - ").append(mapEntry.getKey()).append(": ")
+                        .append(fileDifference.get(mapEntry.getKey()).get("oldValue")).append("\n");
+                sb.append("  + ").append(mapEntry.getKey()).append(": ")
+                        .append(fileDifference.get(mapEntry.getKey()).get("newValue")).append("\n");
+            }
+            if (fileDifference.get(mapEntry.getKey()).get("type").equals("unchanged")) {
+                sb.append("    ").append(mapEntry.getKey()).append(": ")
+                        .append(fileDifference.get(mapEntry.getKey()).get("oldValue")).append("\n");
+            }
+        }
+        return sb.append("}").toString();
+    }
+}
