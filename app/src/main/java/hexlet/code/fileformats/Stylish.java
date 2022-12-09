3cc3b8b8700179f5e376eeb0ab1d81705adf75c0
Kupchinskii Evgenii <kupchinskiievgenii81@gmail.com>
2022-12-09 18:38:04 +0300

    code refactoring

--- app/src/main/java/hexlet/code/fileformats/Stylish.java
+++ app/src/main/java/hexlet/code/fileformats/Stylish.java
@@ -7,23 +7,24 @@ public class Stylish {
     public static String stylishFormat(Map<String, Map<String, Object>> fileDifference) throws IOException {
         StringBuilder sb = new StringBuilder("{\n");
         for (Map.Entry<String, Map<String, Object>> mapEntry : fileDifference.entrySet()) {
+            StringBuilder oldValueBuild = new StringBuilder();
+            oldValueBuild.append(mapEntry.getKey()).append(": ")
+                    .append(fileDifference.get(mapEntry.getKey()).get("oldValue")).append("\n");
+            StringBuilder newValueBuild = new StringBuilder();
+            newValueBuild.append(mapEntry.getKey()).append(": ")
+                    .append(fileDifference.get(mapEntry.getKey()).get("newValue")).append("\n");
             if (fileDifference.get(mapEntry.getKey()).get("type").equals("added")) {
-                sb.append("  + ").append(mapEntry.getKey()).append(": ")
-                        .append(fileDifference.get(mapEntry.getKey()).get("newValue")).append("\n");
+                sb.append("  + ").append(newValueBuild);
             }
             if (fileDifference.get(mapEntry.getKey()).get("type").equals("removed")) {
-                sb.append("  - ").append(mapEntry.getKey()).append(": ")
-                        .append(fileDifference.get(mapEntry.getKey()).get("oldValue")).append("\n");
+                sb.append("  - ").append(oldValueBuild);
             }
             if (fileDifference.get(mapEntry.getKey()).get("type").equals("changed")) {
-                sb.append("  - ").append(mapEntry.getKey()).append(": ")
-                        .append(fileDifference.get(mapEntry.getKey()).get("oldValue")).append("\n");
-                sb.append("  + ").append(mapEntry.getKey()).append(": ")
-                        .append(fileDifference.get(mapEntry.getKey()).get("newValue")).append("\n");
+                sb.append("  - ").append(oldValueBuild);
+                sb.append("  + ").append(newValueBuild);
             }
             if (fileDifference.get(mapEntry.getKey()).get("type").equals("unchanged")) {
-                sb.append("    ").append(mapEntry.getKey()).append(": ")
-                        .append(fileDifference.get(mapEntry.getKey()).get("oldValue")).append("\n");
+                sb.append("    ").append(oldValueBuild);
             }
         }
         sb.append("}");
