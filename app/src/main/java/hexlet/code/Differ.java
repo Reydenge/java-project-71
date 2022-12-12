382f0b6d61abcd20ff76b7394c8cda81da8ba63f
Kupchinskii Evgenii <kupchinskiievgenii81@gmail.com>
2022-12-12 18:48:21 +0300

    fix

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,33 +1,32 @@
 package hexlet.code;
 
-import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
+import java.util.zip.DataFormatException;
 
 public class Differ {
-    public static String generate(String firstFilePath, String secondFilePath, String format) throws IOException {
-        Map<String, Object> firstMap = getFileData(firstFilePath);
-        Map<String, Object> secondMap = getFileData(secondFilePath);
-        var fileDifference = DiffDetector.fileDifference(firstMap, secondMap);
-        return Formatter.compile(fileDifference, format);
+    public static String generate(String firstFilePath, String secondFilePath, String format) throws Exception {
+        var firstData = getFileData(firstFilePath);
+        var secondData = getFileData(secondFilePath);
+        var fileDifference = DiffBuilder.fileDifference(firstData, secondData);
+        return Formatter.format(fileDifference, format);
     }
     public static String generate(String firstFilePath, String secondFilePath) throws Exception {
         return generate(firstFilePath, secondFilePath, "stylish");
     }
 
-    private static Map<String, Object> getFileData(String filepath) throws IOException {
-        File file = new File(filepath);
-        String fileData = Files.readString(file.toPath());
-        String fileFormat = getFileFormat(file);
+    private static Map<String, Object> getFileData(String filepath) throws Exception {
+        String fileData = Files.readString(Paths.get(filepath));
+        String fileFormat = getFileFormat(filepath);
         return Parser.parse(fileData, fileFormat);
     }
 
-    private static String getFileFormat(File file) {
-        if (!file.getPath().contains(".")) {
-            return "";
+    private static String getFileFormat(String filepath) throws Exception {
+        if (!filepath.contains(".")) {
+            throw new DataFormatException();
         }
-        int indexOfLastDot = file.getPath().indexOf(".") + 1;
-        return file.getPath().substring(indexOfLastDot).toLowerCase();
+        int indexOfLastDot = filepath.lastIndexOf(".") + 1;
+        return filepath.substring(indexOfLastDot).toLowerCase();
     }
 }
