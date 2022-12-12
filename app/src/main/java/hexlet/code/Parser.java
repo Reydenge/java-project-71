7ae14b03439202258e31789ef77db25d549149d0
Kupchinskii Evgenii <kupchinskiievgenii81@gmail.com>
2022-12-12 18:55:14 +0300

    checkstyle fix

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -3,15 +3,13 @@ package hexlet.code;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
-import java.io.IOException;
 import java.util.Map;
 
 public class Parser {
     public static Map<String, Object> parse(String fileData, String fileFormat) throws Exception {
         return switch (fileFormat) {
-            case "json" -> new ObjectMapper().readValue(fileData, new TypeReference<>() {} );
-            case "yml" -> new ObjectMapper(new YAMLFactory()).readValue(fileData, new TypeReference<>() {} );
+            case "json" -> new ObjectMapper().readValue(fileData, new TypeReference<>() { });
+            case "yml" -> new ObjectMapper(new YAMLFactory()).readValue(fileData, new TypeReference<>() { });
             default -> throw new RuntimeException("Transmitted file format '" + fileFormat + "' is unsupported");
         };
     }
