c35cb742d23c9293e1969afa19bb1264ce01401b
Kupchinskii Evgenii <kupchinskiievgenii81@gmail.com>
2022-12-12 18:49:34 +0300

    fix

--- app/src/main/java/hexlet/code/fileformats/Plain.java
+++ app/src/main/java/hexlet/code/fileformats/Plain.java
@@ -7,17 +7,21 @@ public class Plain {
         StringBuilder sb = new StringBuilder();
 
         for (Map.Entry<String, Map<String, Object>> mapEntry : fileDifference.entrySet()) {
-            if (fileDifference.get(mapEntry.getKey()).get("type").equals("added")) {
+            var newValue = returnComplexValue(mapEntry.getValue().get("newValue"));
+            var oldValue = returnComplexValue(mapEntry.getValue().get("oldValue"));
+            String typeAdded = "added";
+            String typeRemoved = "removed";
+            String typeChanged = "changed";
+            if (fileDifference.get(mapEntry.getKey()).get("type").equals(typeAdded)) {
                 sb.append("Property '").append(mapEntry.getKey()).append("' was added with value: ")
-                        .append(returnComplexValue(mapEntry.getValue().get("newValue"))).append("\n");
+                        .append(newValue).append("\n");
             }
-            if (fileDifference.get(mapEntry.getKey()).get("type").equals("removed")) {
+            if (fileDifference.get(mapEntry.getKey()).get("type").equals(typeRemoved)) {
                 sb.append("Property '").append(mapEntry.getKey()).append("' was removed\n");
             }
-            if (fileDifference.get(mapEntry.getKey()).get("type").equals("changed")) {
+            if (fileDifference.get(mapEntry.getKey()).get("type").equals(typeChanged)) {
                 sb.append("Property '").append(mapEntry.getKey()).append("' was updated. From ")
-                        .append(returnComplexValue(mapEntry.getValue().get("oldValue"))).append(" to ")
-                        .append(returnComplexValue(mapEntry.getValue().get("newValue"))).append("\n");
+                        .append(oldValue).append(" to ").append(newValue).append("\n");
             }
         }
         sb.deleteCharAt(sb.lastIndexOf("\n"));
