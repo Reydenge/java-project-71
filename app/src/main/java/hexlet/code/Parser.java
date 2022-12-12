590d5773e0cc0c45aab4f1b4b0b8f06a4ac2b6c8
Kupchinskii Evgenii <kupchinskiievgenii81@gmail.com>
2022-12-12 18:49:20 +0300

    code refactoring

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -8,11 +8,11 @@ import java.io.IOException;
 import java.util.Map;
 
 public class Parser {
-    public static Map<String, Object> parse(String fileData, String fileFormat) throws IOException {
-        ObjectMapper objectMapper = identifyFileFormat(fileFormat);
-        return objectMapper.readValue(fileData, new TypeReference<>() { });
-    }
-    private static ObjectMapper identifyFileFormat(String fileFormat) {
-        return "json".equals(fileFormat) ? new ObjectMapper() : new ObjectMapper(new YAMLFactory());
+    public static Map<String, Object> parse(String fileData, String fileFormat) throws Exception {
+        return switch (fileFormat) {
+            case "json" -> new ObjectMapper().readValue(fileData, new TypeReference<>() {} );
+            case "yml" -> new ObjectMapper(new YAMLFactory()).readValue(fileData, new TypeReference<>() {} );
+            default -> throw new RuntimeException("Transmitted file format '" + fileFormat + "' is unsupported");
+        };
     }
 }
