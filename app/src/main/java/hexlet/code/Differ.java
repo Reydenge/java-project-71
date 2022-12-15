88e444a6f68bd18caccaa0b4ab4ed413c569c7a9
Kupchinskii Evgenii <kupchinskiievgenii81@gmail.com>
2022-12-15 17:31:13 +0300

    rename of class and fields

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,17 +6,17 @@ import java.util.Map;
 import java.util.zip.DataFormatException;
 
 public class Differ {
-    public static String generate(String firstFilePath, String secondFilePath, String format) throws Exception {
-        var firstData = getFileData(firstFilePath);
-        var secondData = getFileData(secondFilePath);
-        var fileDifference = DiffBuilder.fileDifference(firstData, secondData);
-        return Formatter.format(fileDifference, format);
+    public static String generate(String firstPath, String secondPath, String format) throws Exception {
+        var firstData = getData(firstPath);
+        var secondData = getData(secondPath);
+        var difference = DiffBuilder.getDifference(firstData, secondData);
+        return Formatter.format(difference, format);
     }
     public static String generate(String firstFilePath, String secondFilePath) throws Exception {
         return generate(firstFilePath, secondFilePath, "stylish");
     }
 
-    private static Map<String, Object> getFileData(String filepath) throws Exception {
+    private static Map<String, Object> getData(String filepath) throws Exception {
         String fileData = Files.readString(Paths.get(filepath));
         String fileFormat = getFileFormat(filepath);
         return Parser.parse(fileData, fileFormat);
