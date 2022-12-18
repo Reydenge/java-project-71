c39bd48d840c27792cabff15629a4968b40095cd
Kupchinskii Evgenii <kupchinskiievgenii81@gmail.com>
2022-12-18 22:11:08 +0300

    refactoring

--- app/src/main/java/hexlet/code/formats/Stylish.java
+++ app/src/main/java/hexlet/code/formats/Stylish.java
@@ -1,50 +1,33 @@
 package hexlet.code.formats;
 
-import hexlet.code.Utils;
-import java.io.IOException;
+
 import java.util.Map;
 
 public class Stylish {
-    enum Types {
-        added,
-        removed,
-        changed,
-        unchanged;
-    }
-    public static String stylishFormat(Map<String, Map<String, Object>> fileDifference) throws IOException {
+    public static String stylishFormat(Map<String, Map<String, Object>> fileDifference) throws Exception {
         StringBuilder sb = new StringBuilder("{\n");
         for (Map.Entry<String, Map<String, Object>> mapEntry : fileDifference.entrySet()) {
-            if (getCurrentValue(mapEntry, fileDifference).equals(Types.added.toString())) {
-                sb.append("  + ").append(getNewValue(mapEntry));
-            }
-            if (getCurrentValue(mapEntry, fileDifference).equals(Types.removed.toString())) {
-                sb.append("  - ").append(getOldValue(mapEntry));
-            }
-            if (getCurrentValue(mapEntry, fileDifference).equals(Types.changed.toString())) {
-                sb.append("  - ").append(getOldValue(mapEntry));
-                sb.append("  + ").append(getNewValue(mapEntry));
-            }
-            if (getCurrentValue(mapEntry, fileDifference).equals(Types.unchanged.toString())) {
-                sb.append("    ").append(getOldValue(mapEntry));
+            String valueType = (String) mapEntry.getValue().get("type");
+            Object oldValue = mapEntry.getValue().get("oldValue");
+            Object newValue = mapEntry.getValue().get("newValue");
+            switch (valueType) {
+                case "added" -> sb.append("  + ").append(getValue(mapEntry, newValue));
+                case "removed" -> sb.append("  - ").append(getValue(mapEntry, oldValue));
+                case "changed" -> {
+                    sb.append("  - ").append(getValue(mapEntry, oldValue));
+                    sb.append("  + ").append(getValue(mapEntry, newValue));
+                }
+                case "unchanged" -> sb.append("    ").append(getValue(mapEntry, oldValue));
+                default -> throw new RuntimeException();
             }
         }
         sb.append("}");
         return sb.toString();
     }
-    private static StringBuilder getOldValue(Map.Entry<String, Map<String, Object>> mapEntry) {
+    private static String getValue(Map.Entry<String, Map<String, Object>> mapEntry, Object requiredValue) {
         StringBuilder result = new StringBuilder();
-        result.append(mapEntry.getKey()).append(": ")
-                .append(Utils.normalizeValueForStylish(mapEntry.getValue().get("oldValue"))).append("\n");
-        return result;
-    }
-    private static StringBuilder getNewValue(Map.Entry<String, Map<String, Object>> mapEntry) {
-        StringBuilder result = new StringBuilder();
-        result.append(mapEntry.getKey()).append(": ")
-                .append(Utils.normalizeValueForStylish(mapEntry.getValue().get("newValue"))).append("\n");
-        return result;
-    }
-    private static Object getCurrentValue(Map.Entry<String, Map<String, Object>> mapEntry,
-                                          Map<String, Map<String, Object>> fileDifference) {
-        return fileDifference.get(mapEntry.getKey()).get("type");
+        String key = mapEntry.getKey();
+        result.append(key).append(": ").append(String.valueOf(requiredValue)).append("\n");
+        return result.toString();
     }
 }
