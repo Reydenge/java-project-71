c997d26075175c202e8b602195ec764dca3f4f1d
Kupchinskii Evgenii <kupchinskiievgenii81@gmail.com>
2022-12-18 22:32:30 +0300

    fix

--- app/src/main/java/hexlet/code/formats/Plain.java
+++ app/src/main/java/hexlet/code/formats/Plain.java
@@ -16,7 +16,7 @@ public class Plain {
                 case "removed" -> sb.append("Property '").append(mapEntry.getKey()).append("' was removed\n");
                 case "changed" -> sb.append("Property '").append(mapEntry.getKey()).append("' was updated. From ")
                         .append(oldValue).append(" to ").append(newValue).append("\n");
-                default -> throw new RuntimeException();
+                default -> throw new Exception();
             }
         }
         sb.deleteCharAt(sb.lastIndexOf("\n"));
