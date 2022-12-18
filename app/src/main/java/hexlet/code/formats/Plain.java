07febabc5dd13890bdc6381c2e58104a61ec2a96
Kupchinskii Evgenii <kupchinskiievgenii81@gmail.com>
2022-12-18 22:10:37 +0300

    refactoring

--- app/src/main/java/hexlet/code/formats/Plain.java
+++ app/src/main/java/hexlet/code/formats/Plain.java
@@ -1,34 +1,36 @@
 package hexlet.code.formats;
 
 import java.util.Map;
-import hexlet.code.Utils;
 
 public class Plain {
-    enum Types {
-        added,
-        removed,
-        changed;
-
-    }
-    public static String plainFormat(Map<String, Map<String, Object>> fileDifference) {
+    public static String plainFormat(Map<String, Map<String, Object>> fileDifference) throws Exception {
         StringBuilder sb = new StringBuilder();
 
         for (Map.Entry<String, Map<String, Object>> mapEntry : fileDifference.entrySet()) {
-            var newValue = Utils.normalizeValueForPlain(mapEntry.getValue().get("newValue"));
-            var oldValue = Utils.normalizeValueForPlain(mapEntry.getValue().get("oldValue"));
-            if (fileDifference.get(mapEntry.getKey()).get("type").equals(Types.added.toString())) {
-                sb.append("Property '").append(mapEntry.getKey()).append("' was added with value: ")
+            String valueType = (String) fileDifference.get(mapEntry.getKey()).get("type");
+            var newValue = normalize(mapEntry.getValue().get("newValue"));
+            var oldValue = normalize(mapEntry.getValue().get("oldValue"));
+            switch (valueType) {
+                case "added" -> sb.append("Property '").append(mapEntry.getKey()).append("' was added with value: ")
                         .append(newValue).append("\n");
-            }
-            if (fileDifference.get(mapEntry.getKey()).get("type").equals(Types.removed.toString())) {
-                sb.append("Property '").append(mapEntry.getKey()).append("' was removed\n");
-            }
-            if (fileDifference.get(mapEntry.getKey()).get("type").equals(Types.changed.toString())) {
-                sb.append("Property '").append(mapEntry.getKey()).append("' was updated. From ")
+                case "removed" -> sb.append("Property '").append(mapEntry.getKey()).append("' was removed\n");
+                case "changed" -> sb.append("Property '").append(mapEntry.getKey()).append("' was updated. From ")
                         .append(oldValue).append(" to ").append(newValue).append("\n");
+                default -> throw new RuntimeException();
             }
         }
         sb.deleteCharAt(sb.lastIndexOf("\n"));
         return sb.toString();
+    }
+    public static Object normalize(Object value) {
+        if (value == null) {
+            return "null";
+        } else if (value instanceof Integer || value instanceof Boolean) {
+            return value;
+        } else if (value instanceof String) {
+            return "'" + value + "'";
+        } else {
+            return "[complex value]";
+        }
     }
 }
